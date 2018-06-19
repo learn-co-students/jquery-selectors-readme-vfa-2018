dbd7e2159b70bd9d9a3fc7ef33a9e03d66016ea8
James Peacock <james.hoke.peacock@gmail.com>
2018-06-19 16:51:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function pargraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics div')
+
+}
+
+function firstListItem() {
+  return $('#pic-list li:first')
+}
