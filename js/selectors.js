c9ad15a63ddcacf5bf43966ce4c942c26d254a46
Vikram Patel <vikramdhnpatel@gmail.com>
2018-06-28 00:07:36 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+
+function paragraphSelector() {
+ if (name === '<p>') {
+   $('p')
+ }
+}
+
+function lastImageSelector() {
+ if (name === '<img>') {
+   $('img')
+ }
+}
+
+function ninjaBabySelector() {
+   $('parent-tag child-tag:first-child')
+}
+
+
+function divSelector() {
+   $('div img:first-child')
+}
+
+
+function firstListItem() {
+   $('ul:pic-list')
+}
