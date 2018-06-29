dc60dd3e2169d77f0a900c84eba45099e09bc68d
Danielle Bennett <dab621@stern.nyu.edu>
2018-06-29 06:34:11 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,6 @@
 // declare your functions here...
+paragraphSelector = $('p')
+lastImageSelector = $('img:last')
+ninjaBabySelector = $("#ninja-baby")
+divSelector = $(".pics")
+firstListItem = $("pic-list ul:first-child")
