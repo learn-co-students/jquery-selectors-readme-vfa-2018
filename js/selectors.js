415a410fa76276398b4ffb48bcfe0cd96c494225
Danielle Deavens <danielledeavens@gmail.com>
2018-06-29 13:00:48 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+$('p');
+  return;
+}
+
+function lastImageSelector() {
+$('img:last');
+  return;
+}
+
+function ninjaBabySelector() {
+ $('.pics:first-child'); $('.pics:second-child');
+  return;
+}
+
+function firstListItemSelector() {
+$('.pics .pic-list:first-child');
+  return;
+}
+
+function divSelector() {
+$('#baby-ninja');
+  return;
+}
