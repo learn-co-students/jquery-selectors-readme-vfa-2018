c997ecf70de0af34f7df7a3da6898a719cedbb67
Shawma Chun <shawmachun@gmail.com>
2018-07-02 12:36:40 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('div.pics');
+}
+
+function firstListItem() {
+  var itemOne = $('#pic-list ul:first-child')
+  return itemOne
+}
